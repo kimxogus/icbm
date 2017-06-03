b1257a4af000c434cf1a2d827033b4eb575b13b0
kimxogus <kgyoo8232@gmail.com>
2017-06-03 21:22:56 +0900

    add bashrc

--- src/add/add.js
+++ src/add/add.js
@@ -16,6 +16,8 @@ const success: string = 'success';
 const fail: string = 'fail';
 const unsupported: string = 'unsupported';
 
+export type responseType = alreadyAdded | success | fail | unsupported;
+
 export const response = {
   alreadyAdded,
   success,
@@ -23,30 +25,33 @@ export const response = {
   unsupported,
 };
 
-export default (
-  file: string,
-  filePath: ?string
-): alreadyAdded | success | fail | unsupported => {
+const addAction = (name: string, srcPach: string): responseType => {
+  ensureFileSync(srcPach);
+  const stat = fs.lstatSync(srcPach);
+  if (stat.isSymbolicLink()) {
+    return alreadyAdded;
+  }
+  const addedFilePath = path.join(fileDir, name);
+  copySync(srcPach, addedFilePath);
+  removeSync(srcPach);
+  ensureSymlinkSync(addedFilePath, srcPach);
+  return success;
+};
+
+export default (file: string, filePath: ?string): responseType => {
   mkdirpSync(fileDir);
 
+  let srcPath = null;
+  const homePath = getEnvVar('HOME');
   switch (file) {
     case 'bash_profile':
-      const actualFilePath = defaultTo(
-        filePath,
-        path.join(getEnvVar('HOME'), '.bash_profile')
-      );
-      ensureFileSync(actualFilePath);
-      const stat = fs.lstatSync(actualFilePath);
-      if (stat.isSymbolicLink()) {
-        return alreadyAdded;
-      }
-      const addedFilePath = path.join(fileDir, 'bash_profile');
-      copySync(actualFilePath, addedFilePath);
-      removeSync(actualFilePath);
-      ensureSymlinkSync(addedFilePath, actualFilePath);
-      return success;
-
+      srcPath = defaultTo(filePath, path.join(homePath, '.bash_profile'));
+      break;
+    case 'bashrc':
+      srcPath = defaultTo(filePath, path.join(homePath, '.bashrc'));
+      break;
     default:
       return unsupported;
   }
+  return addAction(file, srcPath);
 };
