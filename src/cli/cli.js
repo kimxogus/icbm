e7f0cd72e9c5482d2327fab47c88ca46ae018380
kimxogus <kgyoo8232@gmail.com>
2017-06-03 17:38:40 +0900

    set version

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,7 +1,12 @@
 import program from 'commander';
-
 import config from './config';
+import packageJson from '../../package.json';
 
+// Version
+program.version(packageJson.version);
+
+// Commands
 program.command('config <type> [key] [value]').action(config);
 
+// Parse and execute
 program.parse(process.argv);
--- src/config/__tests__/keys.js
+++ src/config/__tests__/keys.js
@@ -10,6 +10,8 @@ test('validate key and value pairs', () => {
   expect(keys.validate('repository.type', 'gist')).toBe(true);
   expect(keys.validate('repository.type', 'github')).toBe(true);
   expect(keys.validate('repository.type', 'wrong')).toBe(false);
-  expect(keys.validate('repository.url', 'https://github.com/kimxogus/xo.git')).toBe(true);
+  expect(
+    keys.validate('repository.url', 'https://github.com/kimxogus/xo.git')
+  ).toBe(true);
   expect(keys.validate('repository.githubToken', 'tokentoken')).toBe(true);
 });
