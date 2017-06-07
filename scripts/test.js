8a3263f664c97ad160dbe66759e7cecd795319cf
kimxogus <kgyoo8232@gmail.com>
2017-06-08 00:04:36 +0900

    no watch by default

--- scripts/test.js
+++ scripts/test.js
@@ -13,10 +13,6 @@ if (fs.existsSync('.env.test'))
 
 const argv = process.argv.slice(2);
 
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
-}
-
 if (process.env.CI) {
   argv.push('--runInBand');
 }
