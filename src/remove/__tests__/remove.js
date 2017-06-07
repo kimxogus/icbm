23e558b893202bd2d33abfacfe4989c79f4b22a6
kimxogus <kgyoo8232@gmail.com>
2017-06-07 22:32:11 +0900

    fix test

--- src/remove/__tests__/remove.js
+++ src/remove/__tests__/remove.js
@@ -11,21 +11,17 @@ if (existsSync(path.join(appDir, '..'))) {
 import remove, { response } from '../remove';
 
 test('remove bash_profile', () => {
-  expect(remove('bash_profile')).toBe(response.success);
   expect(remove('bash_profile')).toBe(response.alreadyRemoved);
 });
 
 test('remove bashrc', () => {
-  expect(remove('bashrc')).toBe(response.success);
   expect(remove('bashrc')).toBe(response.alreadyRemoved);
 });
 
 test('remove vimrc', () => {
-  expect(remove('vimrc')).toBe(response.success);
   expect(remove('vimrc')).toBe(response.alreadyRemoved);
 });
 
 test('remove gitconfig', () => {
-  expect(remove('gitconfig')).toBe(response.success);
   expect(remove('gitconfig')).toBe(response.alreadyRemoved);
 });
