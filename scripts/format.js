261bd48bd98898dc838cc0cd6d1c55d6412fb290
kimxogus <kgyoo8232@gmail.com>
2017-06-12 21:31:45 +0900

    no code format on ci

---
+++ scripts/format.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import { CLIEngine } from 'eslint';
+
+if (!process.env.CI) {
+  new CLIEngine({ fix: true, useEslintrc: true })
+    .executeOnFiles(['.'])
+    .results.filter(r => r.output)
+    .forEach(r => fs.writeFile(r.filePath, r.output, { encoding: 'utf8' }));
+}
