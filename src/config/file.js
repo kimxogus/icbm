54e8dd5ef5013d08021581c106cda39ebcae7142
Taehyun Kim <kgyoo8232@gmail.com>
2018-10-28 13:55:34 +0900

    bump minor dependencies

--- src/config/file.js
+++ src/config/file.js
@@ -24,7 +24,9 @@ export const getConfigs = (...keys: ?string): object => {
       .type()
       .toLowerCase()
       .indexOf('windows') !== -1
-      ? fs.existsSync(configFile) ? readJsonSync(configFile) : {}
+      ? fs.existsSync(configFile)
+        ? readJsonSync(configFile)
+        : {}
       : rc(appName);
   const config: object = omit({ ...defaultConfig, ...rcFile }, [
     '_',
