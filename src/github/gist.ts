f3ae165c44d3556e9268269f573ce72718f32c15
kimxogus <kgyoo8232@gmail.com>
2020-09-22 23:34:21 +0900

    restore authenticate function

--- src/github/gist.ts
+++ src/github/gist.ts
@@ -3,7 +3,7 @@ import {
   GistsGetResponseData,
   GistsUpdateResponseData,
 } from '@octokit/types';
-import github from './github';
+import github, { authenticate } from './github';
 import { getConfig, setConfig } from '../config';
 
 export interface File {
@@ -20,7 +20,9 @@ export interface CreateOption {
   public?: boolean;
 }
 
-export const create = (option?: CreateOption): Promise<any> => {
+export async function create(option?: CreateOption): Promise<any> {
+  await authenticate();
+
   return github.gists
     .create({
       description: 'Gist for icbm',
@@ -28,23 +30,25 @@ export const create = (option?: CreateOption): Promise<any> => {
       ...option,
     })
     .then(res => setConfig('repository.gist', res.data.id));
-};
+}
 
-export const get = (
+export async function get(
   id?: string
-): Promise<OctokitResponse<GistsGetResponseData>> => {
+): Promise<OctokitResponse<GistsGetResponseData>> {
   id = id || String(getConfig('repository.gist'));
 
   if (!id || !id.length) return Promise.reject({ message: 'ID is empty' });
 
   return github.gists.get({ gist_id: id });
-};
+}
 
-export const edit = (
+export async function edit(
   files: Files
-): Promise<OctokitResponse<GistsUpdateResponseData>> => {
+): Promise<OctokitResponse<GistsUpdateResponseData>> {
+  await authenticate();
+
   return github.gists.update({
     gist_id: String(getConfig('repository.gist')),
     files,
   });
-};
+}
--- src/github/github.ts
+++ src/github/github.ts
@@ -1,19 +1,23 @@
 import { Octokit } from '@octokit/rest';
+import { createTokenAuth } from '@octokit/auth-token';
 import { getConfig } from '../config';
 import version from '../version';
 
-const token = getConfig('repository.githubToken');
-
-if (!token) {
-  throw new Error(
-    `Set github token using 'icbm config set repository.githubToken <token>'`
-  );
-}
-
 const github = new Octokit({
-  auth: token,
+  auth: getConfig('repository.githubToken'),
   debug: process.env.NODE_ENV !== 'production',
   userAgent: `icbm v${version}, node v${process.versions.node}`,
 });
 
 export default github;
+
+export async function authenticate(): Promise<void> {
+  const token = getConfig('repository.githubToken');
+
+  if (!token)
+    throw new Error(
+      `Set github token using 'icbm config set repository.githubToken <token>'`
+    );
+
+  await createTokenAuth(String(token));
+}
